02f5bca52b0ce2b750366d038b03d4bd98c74a53
Kgluch1 <krzysztof.gluch1@gmail.com>
2024-10-23 16:50:41 +0200

    checking

--- playwright.config.ts
+++ playwright.config.ts
@@ -75,9 +75,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'npm run start',
-    url: 'http://127.0.0.1:4200/',
-    timeout: 120 * 1000
-  },
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://127.0.0.1:4200/',
+  //   timeout: 120 * 1000
+  // },
 });
